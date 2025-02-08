9886f10cf82604f32d8c122f0d1b9978a51daf2b
oshiosann <oshiosann@example.com>
2025-02-08 18:47:25 +0900

    Add tag (modified)

--- main.tf
+++ main.tf
@@ -164,7 +164,7 @@ resource "aws_instance" "web" {
   }
   user_data = file("wordpress.sh")
   tags = {
-    Name      = "web"
+    Name      = "web",
     ManagedBy = "HCP Terraform"
   }
 }
@@ -200,7 +200,7 @@ resource "aws_instance" "frontends" {
   }
 
   tags = {
-    Name      = "Frontend-${each.value}"
+    Name      = "Frontend-${each.value}",
     ManagedBy = "HCP Terraform"
   }
 }
